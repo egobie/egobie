89d92243f44e86334f491572c7ab4fc41fad147c
Bo Huang <bhuang3@Bos-MacBook-Pro.home>
2017-06-01 21:37:38 -0400

    revert "use react-native-splash-screen"
    This reverts commit 668ef334e078c8ec6bfe8f0c410da64f9e01d50e.

--- android/app/src/main/java/com/egobie/MainApplication.java
+++ android/app/src/main/java/com/egobie/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.cboy.rn.splashscreen.SplashScreenReactPackage;
 import com.magus.fblogin.FacebookLoginPackage;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
@@ -33,7 +32,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new SplashScreenReactPackage(),
             new FacebookLoginPackage(),
             new RNGoogleSigninPackage(),
             new RCTCameraPackage(),
